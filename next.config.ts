17cc1490283669744d82c896b2a1b14717be0df0
jifokuboso <jifokuboso@example.com>
2025-06-13 10:51:24 +0800

    Update config for user GitHub Pages site

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/otaki-campsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/otaki-campsite/' : '',
 };
 
 export default nextConfig;
