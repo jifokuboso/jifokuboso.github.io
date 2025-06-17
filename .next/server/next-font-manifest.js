af8c392b9bc4ac31b7a0747b9c9768a44155b3fe
jifokuboso <jifokuboso@example.com>
2025-06-17 19:40:29 +0800

    不要なビルドファイルを削除し、.gitignoreを更新

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/ryosatoh2/Documents/otaki-camp-site/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
